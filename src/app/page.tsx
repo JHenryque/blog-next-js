6eecae5247954d5795682190d190910e5cdc42b3
JHenrique <henryque.pc.game@gmail.com>
2025-10-03 19:37:08 -0300

    Criando um componente de SpinLoader (para loading)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Headers from "@/components/Headers";
+import { SpindLoader } from "@/components/SpinLoader";
 
 export default function HomePage() {
   console.log("Home Page");
   return (
     <section className="conatiner-fluid flex justify-center">
       <div className="container text-center bg-blue-950 py-7">
-        <Headers />
+        <SpindLoader />
       </div>
     </section>
   );
---
+++ src/components/SpinLoader/index.tsx
@@ -0,0 +1,7 @@
+export function SpindLoader() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="h-16 w-16 border-5 border-t-transparent border-blck rounded-full animate-spin"></div>
+    </div>
+  );
+}
